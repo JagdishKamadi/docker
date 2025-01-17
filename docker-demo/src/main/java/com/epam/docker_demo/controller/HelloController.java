a4c95f7ec3e1544c50010d6a5045b4e97e2111fc
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2025-01-17 18:28:09 +0530

    Added the sample docker app

---
+++ docker-demo/src/main/java/com/epam/docker_demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.epam.docker_demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> getUserName() {
+        return ResponseEntity.ok("Hello Jagdish");
+    }
+}
